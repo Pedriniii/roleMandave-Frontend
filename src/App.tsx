495f819139adea088587442e247552044dbedf8a
Pedrini <45887466+Pedriniii@users.noreply.github.com>
2023-09-06 19:37:56 -0300

    commit de teste
    teste

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavigationBar from './components/navBar/navBar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Seu conteúdo principal */}
+      <NavigationBar /> {/* Barra de navegação */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/navBar.tsx
@@ -0,0 +1,46 @@
+// NavigationBar.tsx
+import React, { useState } from 'react';
+import './NavigationBar.css';
+
+function NavigationBar() {
+  const [activeItem, setActiveItem] = useState<string>('home'); // Inicializa com o item 'home' como ativo
+
+  const handleItemClick = (itemName: string) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <nav className="navbar">
+      <div
+        className={`navbar-item ${activeItem === 'home' ? 'navbar-item-active' : ''}`}
+        onClick={() => handleItemClick('home')}
+      >
+        <i className="fas fa-home"></i>
+        <span>Início</span>
+      </div>
+      <div
+        className={`navbar-item ${activeItem === 'transacao' ? 'navbar-item-active' : ''}`}
+        onClick={() => handleItemClick('transacao')}
+      >
+        <i></i>
+        <span>Transações</span>
+      </div>
+      <div
+        className={`navbar-item ${activeItem === 'orcamento' ? 'navbar-item-active' : ''}`}
+        onClick={() => handleItemClick('orcamento')}
+      >
+        <i className="fas fa-user"></i>
+        <span>Orçamento</span>
+      </div>
+      <div
+        className={`navbar-item ${activeItem === 'addPagamento' ? 'navbar-item-active' : ''}`}
+        onClick={() => handleItemClick('addPagamento')}
+      >
+        <i className="fas fa-user"></i>
+        <span>Adcionar Pagamento</span>
+      </div>
+    </nav>
+  );
+}
+
+export default NavigationBar;
