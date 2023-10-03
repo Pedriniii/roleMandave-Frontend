17c02bf982530864f44bb225feefa8eb6974f5d3
Pedrini <45887466+Pedriniii@users.noreply.github.com>
2023-10-03 08:24:44 -0300

    odio
    tsx

--- src/components/orcamentos/formularioOrcamento.tsx
+++ src/components/orcamentos/formularioOrcamento.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './orcamentos.css';
-import ToastManager from '../../toastManager';
 import { Link } from "react-router-dom";
 
 interface FormularioOrcamentoProps {
